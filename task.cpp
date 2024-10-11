1751a9d265817cb1391c55336f455db93147f77d
waqastariq908 <waqasTariq1019@gmail.com>
2024-10-11 11:27:41 +0500

    Initial commit

---
+++ task.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int number1;
+int number2;
+cout<<"enter  number 1:"<< endl;
+cin>>number1;
+cout<<"enter  number 2:"<< endl;
+cin>>number2;
+cout<<"comparing both number:"<<(number1 < number2)<<endl;
+cout<<"comparing both number:"<<(number1 > number2)<<endl;
+cout<<"comparing both number:"<<(number1 <= number2)<<endl;
+cout<<"comparing both number:"<<(number1 >= number2)<<endl;
+cout<<"comparing both number:"<<(number1 != number2)<<endl;
+
+    return 0;
+}
